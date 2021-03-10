672a52c61790843c10ddfada499b81b8ec55859c
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-10 15:34:02 +0800

    adding diseased

---
+++ configs/diseased/resnet50_diseased.py
@@ -0,0 +1,4 @@
+_base_ = [
+    '../_base_/models/resnest50.py', '../_base_/datasets/diseased_bs32_pil_resize.py',
+    '../_base_/schedules/imagenet_bs256_coslr.py', '../_base_/default_runtime.py'
+]
