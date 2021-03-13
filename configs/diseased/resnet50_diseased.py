523098aaf112de5419d3a21141ac3d88b04f37fe
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-13 23:35:47 +0800

    fix numcls

--- configs/diseased/resnet50_diseased.py
+++ configs/diseased/resnet50_diseased.py
@@ -2,3 +2,8 @@ _base_ = [
     '../_base_/models/resnest50.py', '../_base_/datasets/diseased_bs32_pil_resize.py',
     '../_base_/schedules/imagenet_bs256_coslr.py', '../_base_/default_runtime.py'
 ]
+model = dict(
+    head=dict(
+        num_classes=2,
+        topk=(1,))
+)
