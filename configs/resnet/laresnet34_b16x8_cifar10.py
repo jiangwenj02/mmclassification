54398c36230e6eaf3b7df3f17c42bd07dfd28ad3
jiangwenj02 <jiangwenj02@gmail.com>
2021-04-26 15:52:50 +0800

    DEBUG

--- configs/resnet/laresnet34_b16x8_cifar10.py
+++ configs/resnet/laresnet34_b16x8_cifar10.py
@@ -2,3 +2,5 @@ _base_ = [
     '../_base_/models/laresnet34_cifar.py', '../_base_/datasets/cifar10_bs16.py',
     '../_base_/schedules/cifar10_bs128.py', '../_base_/default_runtime.py'
 ]
+
+data = dict(workers_per_gpu=0,)
