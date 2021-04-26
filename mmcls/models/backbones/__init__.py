6ce94369485cc14246c746d90eee08a7cd3d2039
jiangwenj02 <jiangwenj02@gmail.com>
2021-04-26 15:35:35 +0800

    fix

--- mmcls/models/backbones/__init__.py
+++ mmcls/models/backbones/__init__.py
@@ -12,9 +12,10 @@ from .seresnext import SEResNeXt
 from .shufflenet_v1 import ShuffleNetV1
 from .shufflenet_v2 import ShuffleNetV2
 from .vgg import VGG
+from .laresnet import LAResNet
 
 __all__ = [
     'LeNet5', 'AlexNet', 'VGG', 'RegNet', 'ResNet', 'ResNeXt', 'ResNetV1d',
     'ResNeSt', 'ResNet_CIFAR', 'SEResNet', 'SEResNeXt', 'ShuffleNetV1',
-    'ShuffleNetV2', 'MobileNetV2', 'MobileNetv3'
+    'ShuffleNetV2', 'MobileNetV2', 'MobileNetv3', 'LAResNet'
 ]
