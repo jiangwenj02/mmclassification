f506a5444bb2eca53042cf9e42e0708f09084678
jiangwenj02 <jiangwenj02@gmail.com>
2021-08-23 14:46:43 +0800

    fix

--- configs/diseased/shuffle_cancer.py
+++ configs/diseased/shuffle_cancer.py
@@ -1,9 +1,9 @@
 _base_ = [
-    '../_base_/models/resnest50.py', '../_base_/datasets/cancer_bs32_pil_resize.py',
+    '../_base_/models/shufflenet_v2_1x.py', '../_base_/datasets/cancer_bs32_pil_resize.py',
     '../_base_/schedules/imagenet_bs256_coslr.py', '../_base_/default_runtime.py'
 ]
 model = dict(
-    pretrained='torchvision://resnet50',
+    pretrained='torchvision://shufflenetv2_x1.0',
     head=dict(
         num_classes=2,
         topk=(1,))
