b12a9a4ce200abad78ea8306b5b449636ff5f8f0
jiangwenj02 <jiangwenj02@gmail.com>
2021-09-01 21:26:03 +0800

    fix

---
+++ tools/inference.py
@@ -0,0 +1,60 @@
+import os
+import os.path as osp
+import imageio
+import pandas as pd
+import datetime
+from collections import defaultdict
+import json
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib as mpl
+import multiprocessing
+import cv2
+import torch
+from pathlib import Path
+import mmcv
+import time
+from mmcls.apis import init_model, inference_model
+import glob
+
+class Evaluator:
+    def __init__(self, opt):
+
+        self.opt = opt
+        self.saving_root = opt.save_path
+        self.image_root = opt.image_path
+        os.popen('rm -r ' + osp.join(self.saving_root, '*'))
+        os.makedirs(self.saving_root, exist_ok=True)
+
+    def _init_detector(self):
+        self.device = self.opt.device
+
+        # Load model
+        model = init_model(self.opt.config, checkpoint=self.opt.weights[0], device='cpu')
+        self.names = model.CLASSES
+        self.model = model.to(self.device)
+
+    def test_images(self):
+        self._init_detector()
+        for index, image_file in enumerate(glob.glob(self.image_root)):
+            img = mmcv.imread(image_file)
+            # Inference
+            result = inference_model(self.model, img)
+            img = self.model.show_result(img, result, show=False)
+            
+            if result['pred_label'] != 0:
+                filename = image_file.replace(self.image_root, self.saving_root)
+                mmcv.imwrite(img, filename)            
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="video_evaluation")
+    parser.add_argument('--video_path', type=str, default='/data2/qilei_chen/DATA/erosive_ulcer_videos', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--save_path', type=str, default='/data3/zzhang/tmp/erosive_ulcer_videos0615/', help='source')  # file/folder, 0 for webcam
+    args = parser.parse_args()
+    evaluator = Evaluator(args)
+    evaluator.test_images()
+
+
