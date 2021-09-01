ecf4d2722e1663c7a25d4e2b3ad56a5e5a267d76
jiangwenj02 <jiangwenj02@gmail.com>
2021-09-01 12:25:15 +0800

    fix

--- tools/test_robust_single.py
+++ tools/test_robust_single.py
@@ -148,12 +148,12 @@ def main():
             results = copy.deepcopy(results_all[0])
             for idx, item in enumerate(results_all):
                 for k,v in item.items():
-                    if v is not float:
-                        print(k,v,type(v))
+                    if type(v) is np.ndarray:
+                        results[k][idx] = v[idx]
                     # if v is not float and v.shape[0] > 1:
                     #     results[k][idx] = v[idx]
             for k,v in results.items():
-                if v is not float and v.shape[0] > 1:
+                if type(v) is np.ndarray:
                     print(k,v)
                     results[k] = np.mean(results[k])
             f1_best = 0
