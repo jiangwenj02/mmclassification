6ba89abf326e76fc93be03f95904cfbd8c5f5bb2
jiangwenj02 <jiangwenj02@gmail.com>
2021-12-08 15:13:53 +0800

    f

--- configs/diseased/seresnet50_b32x8_stomach.py
+++ configs/diseased/seresnet50_b32x8_stomach.py
@@ -14,6 +14,8 @@ model = dict(
 # yapf:disable
 log_config = dict(interval=1)
 # yapf:enable
-
-# optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
-# load_from = 'https://download.openmmlab.com/mmclassification/v0/se-resnet/se-resnet50_batch256_imagenet_20200804-ae206104.pth' 
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=10, norm_type=2))
+    
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+load_from = 'https://download.openmmlab.com/mmclassification/v0/se-resnet/se-resnet50_batch256_imagenet_20200804-ae206104.pth' 
