745d8f2d32de0abd3d21dd288d5f045afa955dc5
jiangwenj02 <jiangwenj02@gmail.com>
2022-01-11 09:48:50 +0800

    f

--- configs/diseased/seresnet50_b32x8_stomach_mix_hr.py
+++ configs/diseased/seresnet50_b32x8_stomach_mix_hr.py
@@ -12,11 +12,11 @@ model = dict(
 )
 
 data = dict(
-    train=dict(data_prefix='/data2/zinan_xiong/gastritis_0101_mixed_resolution/train'),
+    train=dict(data_prefix='/data2/zinan_xiong/gastritis_0109_mixed_resolution_with_nianye/train'),
     # val=dict(data_prefix='/data2/zinan_xiong/gastritis_1218_high_resolution/test'),
     # test=dict(data_prefix='/data2/zinan_xiong/gastritis_1218_high_resolution/test')
-    val=dict(data_prefix='/data2/zinan_xiong/gastritis_0101_mixed_resolution/test'),
-    test=dict(data_prefix='/data2/zinan_xiong/gastritis_0101_mixed_resolution/test')
+    val=dict(data_prefix='/data2/zinan_xiong/gastritis_0109_mixed_resolution_with_nianye/test'),
+    test=dict(data_prefix='/data2/zinan_xiong/gastritis_0109_mixed_resolution_with_nianye/test')
     )
 
 # yapf:enable
