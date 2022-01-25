517d4d2f12119fffa7442a1b398e80e7d6528b52
jiangwenj02 <jiangwenj02@gmail.com>
2022-01-25 20:43:17 +0800

    f

--- mmcls/apis/test.py
+++ mmcls/apis/test.py
@@ -34,8 +34,8 @@ def single_gpu_test(model,
         if show or out_dir:
             scores = np.vstack(result)
             pred_score = np.max(scores, axis=1)
-            pred_label = np.argmax(scores, axis=1)[0]
-            pred_class = model.CLASSES[pred_label]
+            pred_label = np.argmax(scores, axis=1)
+            
 
             img_metas = data['img_metas'].data[0]
             imgs = tensor2imgs(data['img'], **img_metas[0]['img_norm_cfg'])
@@ -50,8 +50,7 @@ def single_gpu_test(model,
                 img_dir = osp.dirname(img_meta['ori_filename'])
                 img_name = osp.basename(img_meta['ori_filename'])
                 if out_dir:
-                    import pdb
-                    pdb.set_trace()
+                    pred_class = model.CLASSES[pred_label[i]]
                     out_file = osp.join(out_dir, img_dir, pred_class, img_name)
                 else:
                     out_file = None
