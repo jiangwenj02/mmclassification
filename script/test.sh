4b51b65d68daf30d949070fb74b8d7e466e1fbcd
jiangwenj02 <jiangwenj02@gmail.com>
2022-02-09 13:20:17 +0800

    f

--- script/test.sh
+++ script/test.sh
@@ -19,3 +19,17 @@ python tools/test_robust.py configs/diseased/shuffle_stomach_mix_hr.py work_dirs
 
 python tools/test_robust.py configs/diseased/seresnet50_b32x8_stomach_mix_hr.py work_dirs/seresnet50_b32x8_stomach_mix_hr/latest.pth --show-dir work_dirs/seresnet50_b32x8_stomach_mix_hr/result_20220130
 python tools/test_robust.py configs/diseased/shuffle_stomach_mix_hr.py work_dirs/shuffle_stomach_mix_hr/latest.pth --show-dir work_dirs/shuffle_stomach_mix_hr/result_20220130
+
+20220209
+
+CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/diseased/seresnet50_b32x8_stomach_mix_hr.py && \
+python tools/visualizations/vis_cam_config.py configs/diseased/seresnet50_b32x8_stomach_mix_hr.py work_dirs/seresnet50_b32x8_stomach_mix_hr/latest.pth  \
+--target-layers model.backbone.layer4.2  --method GradCAM --save-path work_dirs/seresnet50_b32x8_stomach_mix_hr/grad_cam20220209 && \
+python tools/test_robust.py configs/diseased/seresnet50_b32x8_stomach_mix_hr.py work_dirs/seresnet50_b32x8_stomach_mix_hr/latest.pth  --metrics accuracy precision recall f1_score f2_score support && \
+python tools/test_robust.py configs/diseased/seresnet50_b32x8_stomach_mix_hr.py work_dirs/seresnet50_b32x8_stomach_mix_hr/latest.pth --show-dir work_dirs/seresnet50_b32x8_stomach_mix_hr/result_20220209
+
+CUDA_VISIBLE_DEVICES=1 python tools/train.py configs/diseased/shuffle_stomach_mix_hr.py && \
+python tools/visualizations/vis_cam_config.py configs/diseased/shuffle_stomach_mix_hr.py work_dirs/shuffle_stomach_mix_hr/latest.pth  \
+--target-layers model.backbone.layers.3 --method GradCAM --save-path work_dirs/shuffle_stomach_mix_hr/grad_cam20220209 && \
+python tools/test_robust.py configs/diseased/shuffle_stomach_mix_hr.py work_dirs/shuffle_stomach_mix_hr/latest.pth --metrics accuracy precision recall f1_score f2_score support && \
+python tools/test_robust.py configs/diseased/shuffle_stomach_mix_hr.py work_dirs/shuffle_stomach_mix_hr/latest.pth --show-dir work_dirs/shuffle_stomach_mix_hr/result_20220209
