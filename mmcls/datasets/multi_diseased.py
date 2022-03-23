5a3f123eda9c00306d81b959357627e1ecd1b371
jiangwenj02 <jiangwenj02@gmail.com>
2022-03-23 11:02:45 +0800

    f

--- mmcls/datasets/multi_diseased.py
+++ mmcls/datasets/multi_diseased.py
@@ -5,6 +5,58 @@ import numpy as np
 from .imagenet import ImageNet
 from .builder import DATASETS
 
+def has_file_allowed_extension2(filename, extensions):
+    """Checks if a file is an allowed extension.
+
+    Args:
+        filename (string): path to a file
+
+    Returns:
+        bool: True if the filename ends with a known image extension
+    """
+    filename_lower = filename.lower()
+    return any(filename_lower.endswith(ext) for ext in extensions)
+
+
+def find_folders2(root):
+    """Find classes by folders under a root.
+
+    Args:
+        root (string): root directory of folders
+
+    Returns:
+        folder_to_idx (dict): the map from folder name to class idx
+    """
+    folders = [
+        d for d in os.listdir(root) if os.path.isdir(os.path.join(root, d))
+    ]
+    folders.sort()
+    folder_to_idx = {folders[i]: i for i in range(len(folders))}
+    return folder_to_idx
+
+def get_samples2(root, folder_to_idx, extensions):
+    """Make dataset by walking all images under a root.
+
+    Args:
+        root (string): root directory of folders
+        folder_to_idx (dict): the map from class name to class idx
+        extensions (tuple): allowed extensions
+
+    Returns:
+        samples (list): a list of tuple where each element is (image, label)
+    """
+    samples = []
+    root = os.path.expanduser(root)
+    for folder_name in sorted(list(folder_to_idx.keys())):
+        _dir = os.path.join(root, folder_name)
+        for _, _, fns in sorted(os.walk(_dir)):
+            for fn in sorted(fns):
+                print(fn)
+                if has_file_allowed_extension2(fn, extensions):
+                    path = os.path.join(folder_name, fn)
+                    item = (path, folder_to_idx[folder_name])
+                    samples.append(item)
+    return samples
 
 @DATASETS.register_module()
 class MultiDiseased(ImageNet):
@@ -20,3 +72,33 @@ class MultiDiseased(ImageNet):
         'vascular_changes', 'white_granular_degeneration', 'diverticulum', 'health',
         'mucous_membrane_hyperemia_edema', 'polypide', 'scar', 'ulcer', 'villous_change'
     ]
+
+    def load_annotations(self):
+        if self.ann_file is None:
+            folder_to_idx = find_folders2(self.data_prefix)
+            print(folder_to_idx)
+            samples = get_samples2(
+                self.data_prefix,
+                folder_to_idx,
+                extensions=self.IMG_EXTENSIONS)
+            if len(samples) == 0:
+                raise (RuntimeError('Found 0 files in subfolders of: '
+                                    f'{self.data_prefix}. '
+                                    'Supported extensions are: '
+                                    f'{",".join(self.IMG_EXTENSIONS)}'))
+
+            self.folder_to_idx = folder_to_idx
+        elif isinstance(self.ann_file, str):
+            with open(self.ann_file) as f:
+                samples = [x.strip().rsplit(' ', 1) for x in f.readlines()]
+        else:
+            raise TypeError('ann_file must be a str or None')
+        self.samples = samples
+
+        data_infos = []
+        for filename, gt_label in self.samples:
+            info = {'img_prefix': self.data_prefix}
+            info['img_info'] = {'filename': filename}
+            info['gt_label'] = np.array(gt_label, dtype=np.int64)
+            data_infos.append(info)
+        return data_infos
