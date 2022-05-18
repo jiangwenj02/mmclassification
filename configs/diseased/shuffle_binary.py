0227626e439e26810931807ed6fd0e7df361590e
jiangwenj02 <jiangwenj02@gmail.com>
2022-05-18 10:46:47 +0800

    f

--- configs/diseased/shuffle_binary.py
+++ configs/diseased/shuffle_binary.py
@@ -9,5 +9,15 @@ model = dict(
         topk=(1,))
 )
 
+data = dict(
+    samples_per_gpu=32,
+    workers_per_gpu=8,
+    train=dict(
+        data_prefix='/data3/dataset/XY_Capsule/Whole_Image_Classification/bin_categories/train'),
+    val=dict(
+        data_prefix='/data3/dataset/XY_Capsule/Whole_Image_Classification/bin_categories/test'),
+    test=dict(
+        data_prefix='/data3/dataset/XY_Capsule/Whole_Image_Classification/bin_categories/test'))
+
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 load_from = 'https://download.openmmlab.com/mmclassification/v0/shufflenet_v2/shufflenet_v2_batch1024_imagenet_20200812-5bf4721e.pth' 
